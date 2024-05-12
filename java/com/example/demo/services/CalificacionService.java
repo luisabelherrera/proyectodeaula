7d94dfa7e1a573d8083fcc5401e004fc42af5cc7
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2024-05-12 16:45:37 -0500

    Add files via upload

---
+++ java/com/example/demo/services/CalificacionService.java
@@ -0,0 +1,17 @@
+package com.example.demo.services;
+
+import com.example.demo.model.entities.Calificacion;
+
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface CalificacionService {
+
+    Iterable<Calificacion> listar();
+    Optional<Calificacion> buscarPorId(Long id);
+    Calificacion guardar(Calificacion calificacion);
+    void eliminar(Long id);
+    void actualizar(Calificacion calificacion);
+}
---
+++ java/com/example/demo/services/EstudianteService.java
@@ -0,0 +1,18 @@
+package com.example.demo.services;
+
+import com.example.demo.model.entities.Estudiante;
+
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface EstudianteService {
+    Iterable<Estudiante> listar();
+    Optional<Estudiante> obtenerPorId(Long id);
+    Estudiante crear(Estudiante estudiante);
+    Estudiante actualizar(Long id, Estudiante estudianteDetalles);
+    void eliminar(Long id);
+
+
+}
